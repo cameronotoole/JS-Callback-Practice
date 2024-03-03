da24c04ca6c57a9aac353b4c1de3b08b83f57a80
cameron otoole <cameronotoole72@gmail.com>
2024-03-02 22:44:39 -0500

    character can now move using arrow keys call back (direction)

--- index.js
+++ index.js
@@ -2,7 +2,28 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+//4 separate 'if' statements not if/else-won't run after first condition is met //
+
+function onDirectionChange(direction) {
+    if (direction === null) {
+        character.src = 'assets/green-character/static.gif'
+    }
+    if (direction === 'west') {
+        character.src = 'assets/green-character/west.gif'
+    }
+    if (direction === 'east') {
+        character.src = 'assets/green-character/east.gif'
+    }
+    if (direction === 'north') {
+        character.src = 'assets/green-character/north.gif'
+    }
+    if (direction === 'south') {
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, onDirectionChange)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
